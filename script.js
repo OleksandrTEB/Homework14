22cc15f9791467589a3380b9d9fe1646ad9274f4
Oleksandr Shvab <[svabsasa62@gmail.com]>
2025-06-26 23:39:51 +0200

    Modifided script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ alert(x > y);
 
 x1 = 16;
 y1 = 16;
-alert(x = y);
+alert(x == y);
 
 x2 = 20;
 y2 = 12;
-alert(x != y); 
+alert(x !== y); 
 
 let age = n;
-let votable = (age > 180) ? "Занадто високий" : "Ваш зріст 180см або більше";
+let votable = (age <= 180) ? "Занадто високий" : "Ваш зріст 180см або більше";
 alert(votable);
