c6b04efed22ebe0a48743d65467d27d2cce7f592
Oleksandr Shvab <[svabsasa62@gmail.com]>
2025-06-26 20:51:44 +0200

    Create file

---
+++ script.js
@@ -0,0 +1,15 @@
+x = 8;
+y = 3;
+alert(x > y);
+
+x1 = 16;
+y1 = 16;
+alert(x = y);
+
+x2 = 20;
+y2 = 12;
+alert(x != y); 
+
+let age = n;
+let votable = (age > 180) ? "Занадто високий" : "Ваш зріст 180см або більше";
+alert(votable);
